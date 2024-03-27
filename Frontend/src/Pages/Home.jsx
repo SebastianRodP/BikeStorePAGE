63c5cae7a8993cd0eb8b86a137089a470bb5689a
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-03-27 18:30:33 -0500

    Subo el componente de Home.jsx en el cual se le agrego los componentes de Navbar, Cartas, Footer

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { Navbar} from "../Components/Navbar/Navbar";
+import { Footer } from "../Components/Footer/Footer";
+import { Cartas } from '../Components/Cartas/Cartas';
 
 export const Home = () => {
   return (
-  <Navbar/>
+  <>
+    <Navbar/>
+    <Cartas/>
+    <Footer/>
+
+  </>
   )
 }
