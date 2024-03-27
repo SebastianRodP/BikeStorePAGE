629e3133d53de93d261e77463e8db9491aff291e
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-03-27 18:25:57 -0500

    Subo el componente de Footer.jsx, el cual va a ser utilizado en las diversas páginas

---
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import "./Footer.css";
+import BicicletaNegraFooter from '../../assets/img/imgFooter/BicicletaNegraFooter.png'
+export const Footer = () =>{
+  return(
+    <footer className="pie-pagina"> 
+        <div className="grupo-1">
+            <div className="box">
+                <div className="logo"> 
+                <a href="#">
+                    <img src={BicicletaNegraFooter} alt="Logo de BikeStore"></img>
+                </a>
+            </div>
+            </div>
+
+            <div className="box">
+                <p>Popayán:calle 16 #9N-44<br/>
+                  B/Antnio Nariño <br/>
+                  +57 317 2373826
+                </p>
+                <p>Armenia:Cr 19 # 36N-38<br/>
+                  Local 3 <br/>
+                  +57 305 4159497</p>
+            </div>
+            <div className="box">
+                <p>Pereira:Carrera 12 # 2-08<br/>
+                    B/ popular modelo<br/>
+                   +57 310 8499122</p>
+                <p>Medellin:Mall Indiana <br/>
+                  Local 123 <br/>
+                  +57 304 5692836</p>
+            </div>
+        </div>
+
+    </footer>
+  )
+}
