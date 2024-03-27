9d311f323735207c882dfac04d83b32dafc512b4
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-03-27 18:32:11 -0500

    subo las imagenes del footer y el navbar, entre otros componentes para el funcionamiento de la página

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { Home } from './Pages/Home'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Home/>
+  <Home/>
+
     </>
   )
 }
---
+++ Frontend/src/Components/Cartas/Cartas.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./Cartas.css";
+import catalogo from '../../assets/img/catalogo.png';
+
+
+export const Cartas  = () =>{
+    return (
+      <div className="content">
+
+<div className="caja-bicicletas">
+  <img src={catalogo} alt=""></img>
+  <p id="descuento">-50%</p>
+  <p id="precio"> descuento 1.600.000</p>
+  <div className="detalle-bicicletas">
+    <h2 className="nombre-bici">Bicicleta MTB GW</h2>
+    <p>16 unidades <br/> 3 colores disponibles </p>
+  </div>
+</div>
+</div>
+    
+    )
+}
+
+
+
+
