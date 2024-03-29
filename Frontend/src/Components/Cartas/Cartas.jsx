1d6770dc35bada2b8edd806214a6a546c1ee4665
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-03-29 10:28:41 -0500

    formato

--- Frontend/src/Components/Cartas/Cartas.jsx
+++ Frontend/src/Components/Cartas/Cartas.jsx
@@ -1,27 +1,22 @@
-import React from 'react';
+import React from "react";
 import "./Cartas.css";
-import catalogo from '../../assets/img/catalogo.png';
+import catalogo from "../../assets/img/catalogo.png";
 
-
-export const Cartas  = () =>{
-    return (
-      <div className="content">
-
-<div className="caja-bicicletas">
-  <img src={catalogo} alt=""></img>
-  <p id="descuento">-50%</p>
-  <p id="precio"> descuento 1.600.000</p>
-  <div className="detalle-bicicletas">
-    <h2 className="nombre-bici">Bicicleta MTB GW</h2>
-    <p>16 unidades <br/> 3 colores disponibles </p>
-    <i className="fa-solid fa-house"></i>
-  </div>
-</div>
-</div>
-    
-    )
-}
-
-
-
-
+export const Cartas = () => {
+  return (
+    <div className="content">
+      <div className="caja-bicicletas">
+        <img src={catalogo} alt=""></img>
+        <p id="descuento">-50%</p>
+        <p id="precio"> descuento 1.600.000</p>
+        <div className="detalle-bicicletas">
+          <h2 className="nombre-bici">Bicicleta MTB GW</h2>
+          <p>
+            16 unidades <br /> 3 colores disponibles{" "}
+          </p>
+          <i className="fa-solid fa-house"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
