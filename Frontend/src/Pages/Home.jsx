08b0ac4981475f2da7de831e86bd8797d175dd85
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-03-28 21:21:11 -0500

    agrego en el Home componentes de banner, filtro, cartas

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { Navbar} from "../Components/Navbar/Navbar";
 import { Footer } from "../Components/Footer/Footer";
 import { Cartas } from '../Components/Cartas/Cartas';
+import {Banner} from '../Components/Banner/Banner';
+import {Filtro} from '../Components/Filtro/Filtro'
 
 export const Home = () => {
   return (
   <>
     <Navbar/>
+    <Banner/>
+    <Filtro/>
     <Cartas/>
     <Footer/>
 
