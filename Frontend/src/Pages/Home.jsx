5194d9809cc20950ee8977ef5324046c51f06294
ingrid-arce <160282857+ingrid-arce@users.noreply.github.com>
2024-03-29 15:26:49 -0500

    se agrego en el home el componente que asignara los valores de la base de datos

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Navbar} from "../Components/Navbar/Navbar";
 import { Footer } from "../Components/Footer/Footer";
 import { Cartas } from '../Components/Cartas/Cartas';
 import {Banner} from '../Components/Banner/Banner';
 import {Filtro} from '../Components/Filtro/Filtro'
+import useFetchGetUser from '../../hooks/useFetchGetUser';
 
 export const Home = () => {
+ const {dataUsers}= useFetchGetUser()
   return (
   <>
+ {dataUsers.map((usuario)=> {
+  return(<h1>{usuario.correo}</h1>)
+ })}
     <Navbar/>
     <Banner/>
     <Filtro/>
     <Cartas/>
     <Footer/>
-
   </>
   )
 }
