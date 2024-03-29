452e282b2f5340510d7282b1cfe5c6af70779ca9
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-03-28 21:17:48 -0500

    agrego el componente de banner

---
+++ Frontend/src/Components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Banner.css'
+
+
+export const Banner = () => {
+  return (
+  <div className='centrar'>
+    <div className="banner">
+      <div className="banner_contenido">
+        <p>Bicicletas</p>
+      </div>
+    </div>
+  </div>
+  )
+}
