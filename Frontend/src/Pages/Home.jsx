65ed194a4fdd2d23f0fe4e1406ffb5a9c9037325
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-03-31 18:17:18 -0500

    subo archivos actualizados

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Navbar} from "../Components/Navbar/Navbar";
+import { Navbar } from '../Components/Navbar/Navbar';
 import { Footer } from "../Components/Footer/Footer";
-
-
 
 
 const Home = () => {
