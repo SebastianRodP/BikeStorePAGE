2eec9c2bfcb4045c980e33453b0d47eb06c22ea0
Juan Jose Cespedes Villota <120986217+Destroyetpro@users.noreply.github.com>
2024-04-11 13:04:44 -0500

    Filtro
    filtrar en el input de busqueda

---
+++ Frontend/src/Components/InicioCartas/InicioCartas.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InicioCartas() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default InicioCartas
--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import bicicletaNavbar from "../../assets/img/imgNavbar/bicicletaNavbar.png";
 import carritoDeCompras from "../../assets/img/imgNavbar/carritoDeCompras.png";
 import iniciarSesion from "../../assets/img/imgNavbar/iniciarSesion.png";
+import useFetchGetArticulos from "../../hooks/useFetchGetArticulos";
 
 export const Navbar = () => {
+
+  const { data } = useFetchGetArticulos();
+  const[searchTerm, setSearchTerm] = useState("");
+
   return (
     <header className="header">
       <nav className="nav">
@@ -24,8 +29,40 @@ export const Navbar = () => {
               {" "}
               <i className="fa-solid fa-magnifying-glass"></i>
             </a>
-            <input type="search" placeholder="buscar"></input>
+
+            {/*Filtro de busqueda*/}
+
+                  <input id="searchInput" type="text" placeholder="buscar" onChange={(event) => {
+                      setSearchTerm(event.target.value);
+                    }}
+      />
+            {data && data.length > 0 && (
+              <div>
+                {data
+                  .filter((val) => {
+                    if (searchTerm === "") {
+                      return true; // Devolver true para incluir todos los elementos cuando no hay término de búsqueda
+                    } else if (val.nombre.toLowerCase().includes(searchTerm.toLowerCase())) {
+                      return true; // Devolver true si el nombre del artículo incluye el término de búsqueda
+                    }
+                    return false; // Devolver false para excluir los elementos que no cumplen con el criterio de búsqueda
+                  })
+                  .map((val) => {
+                    return (
+                      <div className="search-result" key={val.id}>
+                        <img src={val.img} alt="" />
+                        <h3>{val.nombre}</h3>
+                        <p className="precio">{val.precio}</p>
+                      </div>
+                    );
+                  })}
+              </div>
+            )}
           </div>
+
+            {/*Menu de navegacion */}
+
+
           <ul className="nav-menu">
             <li className="nav-menu-item">
               <Link to="/bicicletas" className="nav-menu-link nav-link">Bicicletas</Link>
