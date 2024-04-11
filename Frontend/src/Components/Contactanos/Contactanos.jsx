c7b3caa2ed896a6cb8ccb30ad35a9bfd6512c640
ingrid-arce <160282857+ingrid-arce@users.noreply.github.com>
2024-04-10 23:16:06 -0500

    .

--- Frontend/src/Components/Contactanos/Contactanos.jsx
+++ Frontend/src/Components/Contactanos/Contactanos.jsx
@@ -7,10 +7,14 @@ const Contactanos = () => {
         <div className='modal-contenedor'>
          <h1 className='titulo'>contactanos</h1>
          <button className='cerrar'>x</button>
+         <div className='one'>
          <input type="text" placeholder='Nombre'/>
          <input type='email' placeholder='Email' />
+         </div>
+         <div className='two'>
          <input type="text" placeholder='Telefono' />
          <input type="text" placeholder='Empresa' />
+         </div>
          <p>Mensaje</p>
          <input type="text" />
          <button>Enviar</button>
