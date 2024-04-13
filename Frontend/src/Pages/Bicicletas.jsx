03d7f45fac5fe54546447edd30641e1f385583df
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-04-13 12:15:34 -0500

    importacion de libreria box-icons

--- Frontend/src/Pages/Bicicletas.jsx
+++ Frontend/src/Pages/Bicicletas.jsx
@@ -4,6 +4,7 @@ import Footer from '../Components/Footer/Footer'
 import Banner from '../Components/Banner/Banner'
 import Filtro from '../Components/Filtro/Filtro'
 import Cartas from '../Components/Cartas/Cartas'
+import 'boxicons';
 
 const Bicicletas = () => {
   return (
