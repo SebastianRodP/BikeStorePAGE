e493878d67e5c9f9641ce14a9557ddfff72f20fd
SebastianRodP <sebasrodrigueztian0@gmail.com>
2024-04-14 21:38:20 -0500

    agrego el diseño de el componente marcas

--- Frontend/src/Components/Marcas/Marcas.jsx
+++ Frontend/src/Components/Marcas/Marcas.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import useFetchGetMarcas from '../../hooks/useFetchGetMarcas';
-
+import "../Marcas/Marcas.css"
 
 const Marcas = () => {
   const { dataMarca } = useFetchGetMarcas();
 
   return (
     <div className="marcas-container">
+      <div className="slide-track">
       {dataMarca.map((marca) => (
         <div key={marca.idmarca} className="marca-item">
           <img src={marca.img} alt={marca.marca} />
         </div>
       ))}
+      </div>
     </div>
   );
 };
